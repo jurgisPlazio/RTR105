4cf053fcd90a0ad907c37bbfeae09e81c354bb97
Jurģis <68749926+jurgisPlazio@users.noreply.github.com>
2020-12-21 09:40:20 +0200

    Update 1lab_darbs.cpp

--- 1lab_darbs.cpp
+++ 1lab_darbs.cpp
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <math.h>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
  double mans_exp ( double x ){
  double a , S ,R;
  int k=0;
@@ -11,10 +15,12 @@
      R = x/k;
      a = a*R;
      S = S+a;
+    // printf("%.2f  %.2f  %.2f\n",x,a,S);
      if(k == 999)
      {
          printf("lietotajafunkcija pirmspēdeja vertība − y=mans_exp %.2f =%.2f \n" ,x , S);
      }
+    // printf("%8.4f\t%8.2f\n" , a , S);
    // printf("%.2f\t%8.2f\t%8.2f\n" ,x , a , S);
  }
  return S;
@@ -43,6 +49,14 @@ printf("            Rekurences reizinatajs:    ____________       \n");
 printf("                                                          \n");
 printf("                                            k             \n");
 printf("standarta funkcija− y= %.2f =%.2f \n" ,x , y );
-yy = mans_exp(x);
-printf("lietotajafunkcija − y=mans_exp %.2f =%.2f \n" ,x , yy );
+
+ofstream out("dati.txt");
+for(int i = -20;i<=20;i++)
+{
+     out << i << "   "<< mans_exp(i) << endl;
+}
+out.close();
+
+
+printf("lietotajafunkcija − y=mans_exp %.2f =%.2f \n" ,x , mans_exp(x) );
 }
